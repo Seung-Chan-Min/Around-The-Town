f0192214900763e48c11affdfc1ad5c9b9498519
cse0518 <g787@naver.com>
2021-11-05 02:08:17 +0900

    commit ProductConverter

--- src/main/java/com/prgm/aroundthetown/product/converter/ProductConverter.java
+++ src/main/java/com/prgm/aroundthetown/product/converter/ProductConverter.java
@@ -1,64 +1,31 @@
 package com.prgm.aroundthetown.product.converter;
 
-import com.prgm.aroundthetown.accommodation.dto.AccommodationDto;
+import com.prgm.aroundthetown.accommodation.converter.AccommodationConverter;
 import com.prgm.aroundthetown.accommodation.entity.Accommodation;
-import com.prgm.aroundthetown.leisure.dto.LeisureDto;
+import com.prgm.aroundthetown.leisure.converter.LeisureConverter;
 import com.prgm.aroundthetown.leisure.entity.Leisure;
 import com.prgm.aroundthetown.product.dto.LocationDto;
 import com.prgm.aroundthetown.product.dto.ProductDto;
 import com.prgm.aroundthetown.product.entity.Product;
+import com.prgm.aroundthetown.product.entity.ProductType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ProductConverter {
+    private final AccommodationConverter accommodationConverter;
+    private final LeisureConverter leisureConverter;
 
-    public ProductDto accommodationToDto(final Accommodation accommodation) {
-        final ProductDto productDto = toProductDtoIncludeLocation(accommodation);
-        final AccommodationDto accommodationDto = AccommodationDto.builder()
-                .accommodationName(accommodation.getAccommodationName())
-                .accommodationCategory(accommodation.getAccommodationCategory())
-                .accommodationNotice(accommodation.getAccommodationNotice())
-                .optionNotice(accommodation.getOptionNotice())
-                .guide(accommodation.getGuide())
-                .build();
-        productDto.setAccommodationDto(accommodationDto);
-
-        final LeisureDto leisureDto = LeisureDto.builder().build();
-        productDto.setLeisureDto(leisureDto);
-
-        return productDto;
-    }
-
-    public ProductDto leisureToDto(final Leisure leisure) {
-        final ProductDto productDto = toProductDtoIncludeLocation(leisure);
-
-        final LeisureDto leisureDto = LeisureDto.builder()
-                .category(leisure.getCategory())
-                .leisureInfomation(leisure.getLeisureInformation())
-                .usecase(leisure.getUsecase())
-                .leisureNotice(leisure.getLeisureNotice())
-                .expirationDate(leisure.getExpirationDate())
-                .build();
-        productDto.setLeisureDto(leisureDto);
-
-        final AccommodationDto accommodationDto = AccommodationDto.builder().build();
-        productDto.setAccommodationDto(accommodationDto);
-
-        return productDto;
-    }
-
-    public ProductDto toProductDtoIncludeLocation(final Product product) {
-        final LocationDto locationDto = LocationDto.builder()
-                .howToVisit(product.getLocation().getHowToVisit())
-                .latitude(product.getLocation().getLatitude())
-                .longitude(product.getLocation().getLongitude())
-                .content(product.getLocation().getContent())
-                .build();
-        return ProductDto.builder()
+    public ProductDto toDto(final Product product) {
+        final ProductDto productDto = ProductDto.builder()
                 .productId(product.getProductId())
-                .locationDto(locationDto)
+                .locationDto(LocationDto.builder()
+                        .howToVisit(product.getLocation().getHowToVisit())
+                        .latitude(product.getLocation().getLatitude())
+                        .longitude(product.getLocation().getLongitude())
+                        .content(product.getLocation().getContent())
+                        .build())
                 .region(product.getRegion())
                 .refundRule(product.getRefundRule())
                 .phoneNumber(product.getPhoneNumber())
@@ -66,5 +33,13 @@ public class ProductConverter {
                 .businessAddress(product.getBusinessAddress())
                 .businessName(product.getBusinessName())
                 .build();
+
+        if (product.getProductType().equals(ProductType.ACCOMMODATION)) {
+            productDto.setAccommodationDto(accommodationConverter.toDto((Accommodation) product));
+            return productDto;
+        } else {
+            productDto.setLeisureDto(leisureConverter.toDto((Leisure) product));
+            return productDto;
+        }
     }
 }
